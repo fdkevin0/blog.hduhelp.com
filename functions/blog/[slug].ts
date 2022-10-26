989e11bea79092a22f2605eb0129bec1ae3a9ad2
FDKevin <fdkevinzero@gmail.com>
2022-10-27 06:15:34 +0800

    update

--- functions/blog/[slug].ts
+++ functions/blog/[slug].ts
@@ -1,6 +1,6 @@
 export const onRequestGet: PagesFunction = async ({ request }) => {
     let url = new URL(request.url)
-    url.pathname = "/"
+    url.pathname = "/blog/\[slug\].html"
     let response = await fetch(url.toString())
     return response
 };
